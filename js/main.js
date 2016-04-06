f8df1ab1c0ec22fe4455750a10bf062ca3a9721c
lilier <duooduo@sina.com>
2016-04-06 23:33:00 +0800

    add style

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ $(function () {
 			$(this).attr('style','-webkit-transform:translate3d(0, '+$(this).attr('top')+'px, 0);');
 		});
 
-		var $unit = $('<section class="unit" top="-50" style="-webkit-transform:translate3d(0, 30%, 0);"><img class="face" src="images/1.jpg" alt=""><p class="text">new'+ ++N +'</p></section>');
+		var $unit = $('<section class="unit" top="-50" style="-webkit-transform:translate3d(0, 30%, 0);"><img class="face" src="images/temp/c_cover.jpg" alt=""><p class="text">new'+ ++N +'</p></section>');
 		box.append($unit);
 
 	}, 2000);
