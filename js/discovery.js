6db15f431dec6bbe5e72b0fafff030b7bef17166
bianbian <13811708195@163.com>
2016-04-17 23:11:54 +0800

    fix ajax 1

--- js/discovery.js
+++ js/discovery.js
@@ -1,26 +1,52 @@
 'use strict';
 
+//url地址段最后带参数，例如：'?shareId=10001'
+//获取查询字符串参数
+function getQueryStringArgs() {
+	var qs = (location.search.length > 0 ? location.search.substring(1) : ""),
+		args = {},
+		items = qs.length ? qs.split("&") : [],
+		item = null,
+		name = null,
+		value = null,
+		i = 0,
+		len = items.length;
+	for (i = 0; i < len; i++) {
+		item = items[i].split('=');
+		name = decodeURIComponent(item[0]);
+		value = decodeURIComponent(item[1]);
+		if (name.length) {
+			args[name] = value;
+		}
+	}
+	return args;
+}
 
-//data:{shareId : 10001 }
 $(function(){
 	$.ajax({
 		// url: location.protocol + '//' + location.host + '/share_meeting/info',
 		url: location.protocol + '//' + location.host + '/LEO/js/data/discovery_data.json',
 		dataType: 'json',
+		data: {
+			// shareId: getQueryStringArgs().shareId
+		},
 		success: function(d){
 			if (d.code == 0) {
 				var data = d.data;
+				$('.user-img img').attr('src',data.imageUrl); //封面
 				$('.dis-title-text').html(data.title);  //标题
 				$('.dis-cover-date i').html(data.time); //时间
 				$('.dis-cover-place i').html(data.address); //地址
 
-				$('.dis-share-ul li').eq(0).find('p').html(data.profile);   //关于主持人
-				$('.dis-share-ul li').eq(1).find('p').html(data.content);   //分享会内容
+				var $shareLi = $('.dis-share-ul li');
+				$shareLi.eq(0).find('p').html(data.profile);   //关于主持人
+				$shareLi.eq(1).find('p').html(data.content);   //分享会内容
 
-				$('.dis-speaker-ul li').eq(0).find('img').attr('src',data.imageUrl);
-				$('.dis-speaker-ul li').eq(0).find('dis-speaker-info').find('h3').html(data.nickName);
-				$('.dis-speaker-ul li').eq(0).find('dis-speaker-info').find('span').html(data.profession);
-				$('.dis-speaker-ul li').eq(0).find('p').html(data.profile);
+				var $speakerLi = $('.dis-speaker-ul li');
+				// $speakerLi.eq(0).find('img').attr('src',data.imageUrl);
+				$speakerLi.eq(0).find('h3').html(data.nickName);
+				$speakerLi.eq(0).find('span').html(data.profession);
+				$speakerLi.eq(0).find('p').html(data.profile);
 			}
 		},
 		error: function(e){
---
+++ js/topic_2.js
@@ -0,0 +1,64 @@
+'use strict';
+
+//url地址段最后带参数，例如：'?symposiumId=10001'
+//获取查询字符串参数
+function getQueryStringArgs() {
+	var qs = (location.search.length > 0 ? location.search.substring(1) : ""),
+		args = {},
+		items = qs.length ? qs.split("&") : [],
+		item = null,
+		name = null,
+		value = null,
+		i = 0,
+		len = items.length;
+	for (i = 0; i < len; i++) {
+		item = items[i].split('=');
+		name = decodeURIComponent(item[0]);
+		value = decodeURIComponent(item[1]);
+		if (name.length) {
+			args[name] = value;
+		}
+	}
+	return args;
+}
+
+$(function(){
+	$.ajax({
+		// url: location.protocol + '//' + location.host + '/symposium/info',
+		url: location.protocol + '//' + location.host + '/LEO/js/data/topic_2_data.json',
+		dataType: 'json',
+		data: {
+			// symposiumId: getQueryStringArgs().symposiumId
+		},
+		success: function(d){
+			if (d.code == 0) {
+				var data = d.data;
+				$('.user-img img').attr('src',data.imageUrl); //图片
+				$('.topic-box02 h3').html(data.title);  //标题
+				$('.topic-txt').append('<p>'+ data.content +'</p>'); //内容
+
+				for (var i=0; i < data.proficient.length; i++) {
+					var proficient = data.proficient[i];
+					$('.topic-box03 ul').append('' +
+						'<li>' +
+							'<h4>'+ proficient.title +'</h4>' +
+							'<p class="topic-content">'+ proficient.content +'</p>' +
+							'<div class="topic-author-box clearfix">' +
+								'<figure class="topic-author-img">' +
+									'<img alt="" src="'+ proficient.headUrl +'">' +
+								'</figure>' +
+								'<p class="topic-author-name">'+ proficient.nickName +'</p>' +
+								'<a href="javascript:void(0)" class="topic-author-btn">查看详情</a>' +
+							'</div>' +
+						'</li>'
+					)
+				}
+			}
+		},
+		error: function(e){
+			// alert(JSON.stringify(e));
+		}
+	})
+});
+
+
