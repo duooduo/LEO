c844d765b6ef30b2fc49d7019990c4f149497e85
lilier <duooduo@sina.com>
2016-04-17 19:09:24 +0800

    add style

--- js/main.js
+++ js/main.js
@@ -4,43 +4,24 @@
 'use strict';
 
 $(function () {
-
 	// 弹幕
-	var box = $('.bullet-box');
-	var N = 0;
+	var box = $('.p-bullet-box');
+	upBullet(box);
 	window.setInterval(function () {
-
-		box.find('.unitbox').each(function(){
-			$(this).attr('top',$(this).attr('top') - 50);
-			if($(this).attr('top') <= -340) {
-				this.remove();
-			}
-			$(this).attr('style','-webkit-transform:translate3d(0, '+$(this).attr('top')+'px, 0);');
-		});
-
-		var $unit = $('<section class="unitbox" top="-50" style="-webkit-transform:translate3d(0, 30%, 0);"><div class="unit"><img class="face" src="images/temp/c_cover.jpg" alt=""><p class="text">new'+ ++N +'</p></div></section>');
-		box.append($unit);
-
+		upBullet(box);
 	}, 2000);
 
 	// 心
 	$('.unit').live('click', function(){
 		// 点击效果
-		var $t = $(this);
-		$t.addClass('active');
+		var obj = $(this);
+		obj.addClass('active');
 		window.setTimeout(function () {
-			$t.removeClass('active');
+			obj.removeClass('active');
 		}, 300);
 
 		// 轨迹
-		var k = 20; // 幅度
-		var v = 20; // 速度
-		var w = 2*Math.PI/80; // 周期T=2π/w，w= 2π/T
-
-		// var l = this.parentNode.offsetLeft;
-		// var t = this.parentNode.offsetTop + this.parentNode.getAttribute('top');
 		var moveHeart = $('<i class="heart"></i>');
-
 		moveHeart.attr({
 			left: parseFloat(this.parentNode.offsetLeft),
 			top: parseFloat(this.parentNode.offsetTop + Number(this.parentNode.getAttribute('top')))
@@ -49,29 +30,54 @@ $(function () {
 			top: moveHeart.attr('top') + 'px'
 		});
 		box.append(moveHeart);
-		// console.log(this.parentNode.offsetLeft);
-		// console.log(this.parentNode.offsetTop);
-
-
 		moveHeart.t0 = new Date().getTime();
+		moveHeart.top = parseFloat(obj.parent().css('top'))+parseFloat(obj.parent().attr('top')) + 10;
+		// console.log('top:'+moveHeart.top);
 
-		// moveHeart.top = parseFloat($t.css('top')) + 20;
-		moveHeart.top = parseFloat($t.parent().css('top'))+parseFloat($t.parent().attr('top')) + 10;
-		console.log('top:'+moveHeart.top);
+		moveHeart.dir = Math.random() - 0.5;
+		moveHeart.k = 15 + Math.random()*5; // 幅度 20
+		moveHeart.v = 15 + Math.random()*5; // 速度 20
+		moveHeart.w = 2*Math.PI/(70 + Math.random()*10); // 周期T=2π/w，w= 2π/T
 
+		console.log(moveHeart);
 		moveHeart.timer = window.setInterval(function () {
-			// y=k*sin(w*x)
 			var t1 = new Date().getTime();
-			var x = (t1 - moveHeart.t0)/1000*v;
+			var x = (t1 - moveHeart.t0)/1000*moveHeart.v;
 			if(x > moveHeart.top) {
 				clearInterval(moveHeart.timer);
 				moveHeart.remove();
 			}
-			var y = k*Math.sin(w*x);
-			var s = moveHeart.attr('style');
+			var y = -moveHeart.k*Math.sin(moveHeart.w*x);
+			// var s = moveHeart.attr('style');
 			moveHeart.attr('style','left:'+moveHeart.attr('left')+'px; top:'+moveHeart.attr('top')+'px; -webkit-transform:translate3d('+y+'px,-'+x+'px,0)');
 		}, 100);
+	});
+});
 
+function upBullet(box,N) {
+	box.find('.unitbox').each(function(){
+		var obj = $(this);
+		obj.attr('top',obj.attr('top') - 50);
+		if(obj.attr('top') <= -340) {obj.remove();}
+		obj.attr('style','-webkit-transform:translate3d(0, '+obj.attr('top')+'px, 0);');
 	});
 
-});
+	var $unit = $('<section class="unitbox" top="-50" style="-webkit-transform:translate3d(0, 30%, 0);"><div class="unit"><img class="face" src="images/temp/c_cover.jpg" alt=""><p class="text">new'+ ++N +'</p></div></section>');
+	box.append($unit);
+}
+
+function upHeart(moveHeart) {
+	var k = 15 + Math.random()*5; // 幅度 20
+	var v = 15 + Math.random()*5; // 速度 20
+	var w = 2*Math.PI/80; // 周期T=2π/w，w= 2π/T
+
+	var t1 = new Date().getTime();
+	var x = (t1 - moveHeart.t0)/1000*v;
+	if(x > moveHeart.top) {
+		clearInterval(moveHeart.timer);
+		moveHeart.remove();
+	}
+	var y = k*Math.sin(w*x);
+	// var s = moveHeart.attr('style');
+	moveHeart.attr('style','left:'+moveHeart.attr('left')+'px; top:'+moveHeart.attr('top')+'px; -webkit-transform:translate3d('+y+'px,-'+x+'px,0)');
+}
